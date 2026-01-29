f171e1c950a9230f3b635c50296d65527ddfe784
raziel <tvindzon@gmail.com>
2026-01-29 15:36:06 +0700

    create:  files

---
+++ src/app/bot/models/__init__.py
@@ -0,0 +1,5 @@
+from app.bot.models.base import Base
+from app.bot.models.user import User
+from app.bot.models.genre_executor import GenreExecutor, Genre, Executor
+from app.bot.models.album import Album
+from app.bot.models.song import Song
