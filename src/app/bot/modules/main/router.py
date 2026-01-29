d4b4f917b1b474c874fc78b3262ee4671b7920ce
raziel <tvindzon@gmail.com>
2026-01-29 15:35:38 +0700

    testings: src\app\bot\modules\main\router.py

--- src/app/bot/modules/main/router.py
+++ src/app/bot/modules/main/router.py
@@ -3,6 +3,9 @@ from aiogram.types import Message
 from aiogram.filters.state import StateFilter
 
 from core.response.messages import messages
+from app.bot.db.uow import UnitOfWork
+from app.bot.db.response import SongResponse
+from app.bot.db.db_helper import db_helper
 
 
 router: Router = Router(name="main")
@@ -10,7 +13,7 @@ router: Router = Router(name="main")
 
 @router.message(
     StateFilter(None),
-    F.text == "/start",
+    F.text,
 )
 async def main(
     message: Message,
@@ -18,6 +21,30 @@ async def main(
     get_main_keyboards,
 ) -> None:
     """Отправляет пользователю reply клавиатуру главного меню."""
+    async with UnitOfWork() as uow:
+        user = await uow.users.create_user(
+            name=message.from_user.first_name,
+            telegram=message.chat.id,
+        )
+        user = await uow.users.get_user_by_telegram(telegram=message.chat.id)
+        genres = await uow.genres.get_or_create_genres(titles=["punk", "horror punk"])
+        executor = await uow.executors.create_executor(
+            name="Король и шут", country="Russia", user=user, genres=genres
+        )
+        album = await uow.albums.create_album(
+            executor=executor, title="Жаль нет ружья", year=2002
+        )
+        songs = await uow.songs.create_songs(
+            album=album,
+            song_repsonse=[
+                SongResponse(file_id="ddasdsad", title="песня 1"),
+                SongResponse(file_id="dsadasdsads", title="песня 2"),
+                SongResponse(file_id="dsadыввdsads", title="песня 3")
+            ],
+        )
+        await uow.session.commit()
+
+
     # Удаляет сообщение которое было последним
     try:
         await bot.delete_message(
@@ -27,6 +54,6 @@ async def main(
         pass
 
     await message.answer(
-        text=messages.START_BOT_MESSAGE,
+        text=str("ok"),
         reply_markup=get_main_keyboards,
     )
