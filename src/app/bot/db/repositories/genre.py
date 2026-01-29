150525668356b88b16b0be131da1b81792893ca4
raziel <tvindzon@gmail.com>
2026-01-29 15:32:20 +0700

    create: src\app\bot\db\repositories\genre.py

---
+++ src/app/bot/db/repositories/genre.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.bot.models import Genre
+
+
+class GenreSqlalchemyRepository:
+    model = Genre
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_or_create_genres(self, titles: List[str]):
+        result = await self.session.scalars(
+            select(Genre).where(self.model.title.in_(titles)),
+        )
+        existing = {genre.title: genre for genre in result.all()}
+
+        genres = []
+        for title in titles:
+            if title in existing:
+                genres.append(existing[title])
+            else:
+                genre = self.model(
+                    title=title,
+                )
+                self.session.add(genre)
+                genres.append(genre)
+        await self.session.flush()
+        return genres
