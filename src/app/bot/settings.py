fd4d3a9f908c4e6da34ac036991a6bb3eb102dce
raziel <tvindzon@gmail.com>
2026-01-29 15:28:10 +0700

    refactor:src\app\bot\settings.py

--- src/app/bot/settings.py
+++ src/app/bot/settings.py
@@ -8,6 +8,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class BotSettings(BaseSettings):
     """Общие настройки бота."""
 
+    BOT_DIR: Path = Path(__file__).resolve().parent
+    DB_PATH: Path = BOT_DIR / "data" / "music_play.db"
+
     SERVICE_NAME: str = "bot"
     NAME_FOR_LOG_FOLDER: str = "bot"
 
@@ -15,9 +18,12 @@ class BotSettings(BaseSettings):
     LIST_BOT_COMMANDS: List[BotCommand] = [
         BotCommand(command="start", description="Меню бота")
     ]
+    
+    ASYNC_SQLITE_BASE: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    SQLITE_BASE: str = f"sqlite:///{DB_PATH}"
 
     model_config: SettingsConfigDict = SettingsConfigDict(
-        env_file=Path(__file__).resolve().parent / ".env", extra="ignore"
+        env_file=BOT_DIR / ".env", extra="ignore"
     )
 
 
