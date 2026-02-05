500f22458a15fa496f824c7b536841b5d5b72055
raziel <tvindzon@gmail.com>
2026-02-05 14:32:40 +0700

    refactpr: src\app\bot\modules\admin\childes\add_executor\api\add_executor.py

--- src/app/bot/modules/admin/childes/add_executor/api/add_executor.py
+++ src/app/bot/modules/admin/childes/add_executor/api/add_executor.py
@@ -1,27 +1,16 @@
 from typing import List
-
 from pathlib import Path
-from dataclasses import dataclass
 
-
-@dataclass
-class ParsedAlbum:
-    title: str
-    year: int
-    path: Path
-
-    @classmethod
-    def validate(cls):
-        return f"{cls.year} {cls.title}"
+from app.bot.view_model import AlbumResponse
 
 
 class ApiAddExecutorAPI:
-    def parse_album(self, base_path: Path) -> List[ParsedAlbum]:
+    def parse_album(self, base_path: Path) -> List[AlbumResponse]:
         albums = []
         for album_dir in base_path.iterdir():
             year, title = album_dir.stem.strip("(").split(")")
             title = title.strip().lower()
-            albums.append(ParsedAlbum(title=title, year=int(year), path=album_dir))
+            albums.append(AlbumResponse(title=title, year=int(year), path=album_dir))
         return albums
 
 
