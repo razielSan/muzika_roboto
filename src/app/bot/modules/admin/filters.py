a51f64a8d0d23798195595f1767d58046b65c594
raziel <tvindzon@gmail.com>
2026-02-08 16:12:27 +0700

    feat: src\app\bot\modules\admin\filters.py

--- src/app/bot/modules/admin/filters.py
+++ src/app/bot/modules/admin/filters.py
@@ -1,5 +1,6 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
+from aiogram.filters.callback_data import CallbackData
 
 from app.bot.settings import settings
 
@@ -7,3 +8,7 @@ from app.bot.settings import settings
 class AdminFilter(BaseFilter):
     async def __call__(self, message: Message):
         return str(message.from_user.id) in settings.ADMINS_LIST
+
+
+class BackAdminMenuCallback(CallbackData, prefix="back_admin_menu"):
+    pass
