8cffec01069e47ca0e475bdfee9b22fd9e7e5362
raziel <tvindzon@gmail.com>
2026-02-11 20:02:49 +0700

    feat: src\app\bot\modules\admin\childes\base_music\handlers\base_music.py

--- src/app/bot/modules/admin/childes/base_music/handlers/base_music.py
+++ src/app/bot/modules/admin/childes/base_music/handlers/base_music.py
@@ -6,6 +6,7 @@ from aiogram.filters.state import StateFilter
 
 from app.bot.modules.admin.childes.base_music.settings import settings
 from app.bot.modules.admin.settings import settings as admin_settings
+from app.bot.settings import settings as bot_settings
 from app.bot.modules.admin.childes.base_music.keyboards.inlinle import (
     show_one_album_songs_with_base_executor,
 )
@@ -20,11 +21,19 @@ from app.bot.modules.admin.childes.base_music.filters import (
     BackExecutorCallback,
     BaseMusicCallback,
     BasePlaySongCallback,
+    ScrollingSongsCallback,
+    ScrollingEXecutorsCallback,
+    ScrollingAlbumsCallback,
 )
 from app.bot.modules.admin.childes.base_music.keyboards.inlinle import (
     show_base_executor_collections,
 )
 from app.bot.view_model import ExecutorResponse, AlbumResponse
+from app.bot.modules.admin.childes.base_music.keyboards.response import (
+    LIMIT_SONGS,
+    LIMIT_ALBUMS,
+)
+from core.response.response_data import Result, LoggingData
 
 
 router: Router = Router(name=__name__)
@@ -34,15 +43,18 @@ router: Router = Router(name=__name__)
 async def base_music(call: CallbackQuery) -> None:
     """Возвращает первого исполнителя из базового музыкального хранилища."""
 
-    print("base_music")
-    logging_data = get_loggers(name=settings.NAME_FOR_LOG_FOLDER)
+    logging_data: LoggingData = get_loggers(name=settings.NAME_FOR_LOG_FOLDER)
 
-    result_executor = await base_music_service.show_executor(
+    result_executor: Result = await base_music_service.show_executor(
         get_info_executor=get_info_executor, logging_data=logging_data
     )
     if result_executor.ok:
-        executor: ExecutorResponse = result_executor.data
-        albums_list: List[AlbumResponse] = executor.albums_list
+        executor: ExecutorResponse = result_executor.data.executor
+        albums_list: List[AlbumResponse] = result_executor.data.albums
+        if albums_list:
+            albums_list = albums_list[0:LIMIT_ALBUMS]
+        total_pages: int = result_executor.data.total_pages
+        len_list_albums: int = len(albums_list)
         await call.message.edit_media(
             media=InputMediaPhoto(
                 media=executor.photo_file_id,
@@ -51,6 +63,11 @@ async def base_music(call: CallbackQuery) -> None:
             reply_markup=show_base_executor_collections(
                 list_albums=albums_list,
                 executor_id=executor.executor_id,
+                count_pages_executors=total_pages,
+                current_page_executor=1,
+                limit_albums=LIMIT_ALBUMS,
+                album_position=0,
+                len_list_albums=len_list_albums,
             ),
         )
     else:
@@ -69,22 +86,25 @@ async def base_music(call: CallbackQuery) -> None:
 @router.callback_query(StateFilter(None), BaseMusicCallback.filter())
 async def show_songs_with_album(
     call: CallbackQuery,
-    bot: Bot,
     callback_data: BaseMusicCallback,
 ) -> None:
     """Возвращает альбом со списком песен для исполнителя."""
 
     executor_id: int = callback_data.executor_id
     album_id: int = callback_data.album_id
+    current_page_executor: int = callback_data.current_page_executor
 
-    result = await base_music_service.show_songs_with_album(
+    result: Result = await base_music_service.show_songs_with_album(
         executor_id=executor_id,
         album_id=album_id,
         get_info_album=get_info_album,
     )
     if result.ok:
-        songs = result.data
-        song: SongResponse = result.data[0]
+        songs: List[SongResponse] = result.data
+        len_list_songs: int = len(songs)
+
+        songs = songs[0:LIMIT_SONGS]  # ограничиваем список песен на страгице
+        song: SongResponse = songs[0]
         await call.message.edit_media(
             media=InputMediaPhoto(
                 media=song.album_photo_file_id,
@@ -92,19 +112,186 @@ async def show_songs_with_album(
             ),
             reply_markup=show_one_album_songs_with_base_executor(
                 list_songs=songs,
-                executor_id=song.album_executor_id,
+                executor_id=executor_id,
                 album_id=album_id,
+                len_list_songs=len_list_songs,
+                limit_songs=LIMIT_SONGS,
+                current_page_executor=current_page_executor,
             ),
         )
     else:
         await call.message.edit_media(
             media=InputMediaPhoto(
-                media=song.album_photo_file_id,
+                media=bot_settings.ALBUM_DEFAULT_PHOTO_FILE_ID,
                 caption=result.error.message,
             ),
             reply_markup=show_one_album_songs_with_base_executor(
                 list_songs=[],
-                executor_id=song.album_executor_id,
+                executor_id=executor_id,
+                album_id=album_id,
+            ),
+        )
+
+
+@router.callback_query(StateFilter(None), ScrollingEXecutorsCallback.filter())
+async def scrolling_base_executors(
+    call: CallbackQuery,
+    callback_data: ScrollingEXecutorsCallback,
+    bot: Bot,
+) -> None:
+    """Пролистывает исполнителей."""
+
+    current_page_executor: int = callback_data.current_page
+    logging_data: LoggingData = get_loggers(name=settings.NAME_FOR_LOG_FOLDER)
+
+    result_executor: Result = await base_music_service.show_executor(
+        get_info_executor=get_info_executor,
+        logging_data=logging_data,
+        page_executor=current_page_executor,
+    )
+    if result_executor.ok:
+        executor: ExecutorResponse = result_executor.data.executor
+        albums_list: List[AlbumResponse] = result_executor.data.albums
+        len_list_albums: int = len(albums_list)
+        if albums_list:
+            albums_list = albums_list[0:LIMIT_ALBUMS]
+        
+        total_pages: int = result_executor.data.total_pages
+
+        try:
+            await call.message.edit_media(
+                media=InputMediaPhoto(
+                    media=executor.photo_file_id,
+                    caption=executor.info_executor,
+                ),
+                reply_markup=show_base_executor_collections(
+                    list_albums=albums_list,
+                    executor_id=executor.executor_id,
+                    count_pages_executors=total_pages,
+                    current_page_executor=current_page_executor,
+                    limit_albums=LIMIT_ALBUMS,
+                    album_position=0,
+                    len_list_albums=len_list_albums,
+                ),
+            )
+
+        except Exception as err:
+            print(err)
+            await bot.answer_callback_query(
+                call.id,
+                text="Исполнитель уже загружен",
+                show_alert=True,
+            )
+    else:
+        await call.message.edit_media(
+            media=InputMediaPhoto(
+                media=admin_settings.ADMIN_PANEL_PHOTO_FILE_ID,
+                caption=result_executor.error.message,
+            ),
+            reply_markup=get_keyboards_menu_buttons,
+        )
+
+
+@router.callback_query(StateFilter(None), ScrollingAlbumsCallback.filter())
+async def scrolling_base_albums(
+    call: CallbackQuery,
+    callback_data: ScrollingAlbumsCallback,
+    bot: Bot,
+) -> None:
+    """Пролистывает альбомы."""
+    current_page_executor: int = callback_data.current_page_executor
+    album_position: int = callback_data.position + callback_data.offset
+    logging_data: LoggingData = get_loggers(name=settings.NAME_FOR_LOG_FOLDER)
+
+    result_executor: Result = await base_music_service.show_executor(
+        get_info_executor=get_info_executor,
+        logging_data=logging_data,
+        page_executor=current_page_executor,
+    )
+    if result_executor.ok:
+        executor: ExecutorResponse = result_executor.data.executor
+        albums_list: List[AlbumResponse] = result_executor.data.albums
+        len_list_albums: int = len(albums_list)
+        
+        albums_list = albums_list[album_position : album_position + LIMIT_ALBUMS]
+        total_pages: int = result_executor.data.total_pages
+
+        await call.message.edit_media(
+            media=InputMediaPhoto(
+                media=executor.photo_file_id,
+                caption=executor.info_executor,
+            ),
+            reply_markup=show_base_executor_collections(
+                list_albums=albums_list,
+                executor_id=executor.executor_id,
+                count_pages_executors=total_pages,
+                current_page_executor=current_page_executor,
+                limit_albums=LIMIT_ALBUMS,
+                album_position=album_position,
+                len_list_albums=len_list_albums,
+            ),
+        )
+
+
+@router.callback_query(StateFilter(None), ScrollingSongsCallback.filter())
+async def scrolling_songs_with_album(
+    call: CallbackQuery,
+    callback_data: ScrollingSongsCallback,
+) -> None:
+    """Пролистывает песни из альбома."""
+
+    current_position: int = max(0, callback_data.position + callback_data.offset)  # на
+    # случай ухода в минус
+    executor_id: int = callback_data.executor_id
+    album_id: int = callback_data.album_id
+    current_page_executor: int = callback_data.current_page_executor
+
+    result: Result = await base_music_service.show_songs_with_album(
+        executor_id=executor_id,
+        album_id=album_id,
+        get_info_album=get_info_album,
+    )
+    if result.ok:
+        songs: List[SongResponse] = result.data
+        if not songs:
+            await call.message.edit_media(
+                media=InputMediaPhoto(
+                    media=admin_settings.ADMIN_PANEL_PHOTO_FILE_ID,
+                    caption="У исполнителя нет загруженных песен",
+                ),
+                reply_markup=get_keyboards_menu_buttons,
+            )
+
+        len_list_songs: int = len(songs)
+
+        songs = songs[current_position : current_position + LIMIT_SONGS]
+        song: SongResponse = songs[0]
+        await call.message.edit_media(
+            media=InputMediaPhoto(
+                media=song.album_photo_file_id,
+                caption=song.info_album,
+            ),
+            reply_markup=show_one_album_songs_with_base_executor(
+                list_songs=songs,
+                executor_id=executor_id,
+                album_id=album_id,
+                len_list_songs=len_list_songs,
+                song_position=current_position,
+                limit_songs=LIMIT_SONGS,
+                current_page_executor=current_page_executor,
+            ),
+        )
+
+    else:
+        await call.message.edit_media(
+            media=InputMediaPhoto(
+                media=bot_settings.ALBUM_DEFAULT_PHOTO_FILE_ID,
+                caption=result.error.message,
+            ),
+            reply_markup=show_one_album_songs_with_base_executor(
+                list_songs=[],
+                executor_id=executor_id,
+                album_id=album_id,
             ),
         )
 
@@ -117,18 +304,23 @@ async def back_to_executor(
     """Возвращает назад к исполнителю, при нажатии кнопки."""
 
     executor_id: int = callback_data.executor_id
+    current_page_executor: int = callback_data.current_page_executor
 
-    logging_data = get_loggers(name=settings.NAME_FOR_LOG_FOLDER)
+    logging_data: LoggingData = get_loggers(name=settings.NAME_FOR_LOG_FOLDER)
 
-    result_executor = await base_music_service.back_executor(
+    result_executor: Result = await base_music_service.back_executor(
         get_info_executor=get_info_executor,
         logging_data=logging_data,
-        executor_id=executor_id,
+        current_page_executor=current_page_executor,
     )
 
     if result_executor.ok:
-        albums_list = result_executor.data
-        album = albums_list[0]
+        albums_list: List[AlbumResponse] = result_executor.data
+        len_list_albums: int = len(albums_list)
+        
+        if albums_list:
+            albums_list = albums_list[0:LIMIT_ALBUMS]
+        album: AlbumResponse = albums_list[0]
 
         await call.message.edit_media(
             media=InputMediaPhoto(
@@ -136,7 +328,13 @@ async def back_to_executor(
                 caption=album.info_executor,
             ),
             reply_markup=show_base_executor_collections(
-                list_albums=albums_list, executor_id=executor_id
+                list_albums=albums_list,
+                executor_id=executor_id,
+                count_pages_executors=album.count_executors,
+                current_page_executor=current_page_executor,
+                album_position=0,
+                limit_albums=LIMIT_ALBUMS,
+                len_list_albums=len_list_albums,
             ),
         )
     else:
@@ -157,18 +355,18 @@ async def play_song(
 ) -> None:
     """Скидывает песню для прослушивания."""
 
-    logging_data = get_loggers(name=settings.NAME_FOR_LOG_FOLDER)
+    logging_data: LoggingData = get_loggers(name=settings.NAME_FOR_LOG_FOLDER)
     position: int = callback_data.position
     album_id: int = callback_data.album_id
 
-    result = await base_music_service.play_song(
+    result: Result = await base_music_service.play_song(
         album_id=album_id,
         position=position,
         logging_data=logging_data,
     )
 
     if result.ok:
-        song = result.data
+        song: SongResponse = result.data
         await bot.send_audio(
             chat_id=call.message.chat.id,
             audio=song.file_id,
