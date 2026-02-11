14274a1e330360f127ca47835d44434ba88dc136
raziel <tvindzon@gmail.com>
2026-02-11 20:01:29 +0700

    feat: src\app\bot\core\bot.py

--- src/app/bot/core/bot.py
+++ src/app/bot/core/bot.py
@@ -1,5 +1,20 @@
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram import Bot, Dispatcher
-from app.bot.settings import settings
 
-telegram_bot: Bot = Bot(token=settings.TOKEN)
+from core.logging.api import get_loggers
+
+
+def create_bot(bot_settings, proxy_settings):
+    logging_data = get_loggers(name=bot_settings.NAME_FOR_LOG_FOLDER)
+
+    if proxy_settings.USE_PROXY:
+        session: AiohttpSession = AiohttpSession(proxy=proxy_settings.get_http_url())
+
+        logging_data.info_logger.info(msg="Бот запущен с использванием прокси")
+        return Bot(token=bot_settings.TOKEN, session=session)
+
+    logging_data.info_logger.info("Бот запущен в обычном режиме")
+    return Bot(token=bot_settings.TOKEN)
+
+
 dp: Dispatcher = Dispatcher()
