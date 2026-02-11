6e9664b41ac52aa616b1305f892b68b2fb58516c
raziel <tvindzon@gmail.com>
2026-02-11 20:02:28 +0700

    feat: src\app\bot\modules\admin\childes\base_music\filters.py

--- src/app/bot/modules/admin/childes/base_music/filters.py
+++ src/app/bot/modules/admin/childes/base_music/filters.py
@@ -4,12 +4,16 @@ from aiogram.filters.callback_data import CallbackData
 
 
 class BackExecutorCallback(CallbackData, prefix="admin_back_executor"):
+    """Каллбэк для сценария возврата, по нажатию кнопки, назад к исполнителю c альбомами."""
+
     executor_id: int
+    current_page_executor: int
 
 
 class BaseMusicCallback(CallbackData, prefix="admin_base_music"):
     executor_id: int
     album_id: int
+    current_page_executor: int
 
 
 class BasePlaySongCallback(CallbackData, prefix="admin_play_song"):
@@ -38,6 +42,8 @@ class ConfirmBaseDeleteAlbumCallback(CallbackData, prefix="confirm_admin_del_alb
 
 
 class BaseDeleteSongMenuCallback(CallbackData, prefix="admin_del_song_menu"):
+    """Каллбэк для сценрая показа инлайн меню выбора песен для удаления."""
+
     album_id: int
 
 
@@ -51,8 +57,37 @@ class ConfirmBaseDeleteSongCallback(CallbackData, prefix="confirm_admin_del_song
 
 
 class CancelBaseDeleteSongCallback(CallbackData, prefix="cancel_admin_del_songs"):
+    """Каллбэк для сценария отмены удаления песен."""
+
     pass
 
 
 class CompleteBaseDeleteSongCallback(CallbackData, prefix="compl_admin_del_songs"):
+    """Каллбэк для сценрая удаления песен после подтверждения."""
+
     pass
+
+
+class ScrollingSongsCallback(CallbackData, prefix="scrolling_songs"):
+    """Каллбэк для сценария пролистывания песен в альбоме."""
+
+    executor_id: int
+    album_id: int
+    position: int
+    offset: int
+    current_page_executor: int
+
+
+class ScrollingAlbumsCallback(CallbackData, prefix="scrolling_albums"):
+    """Каллбэк для сценария пролистывания альбомов."""
+
+    executor_id: int
+    position: int
+    offset: int
+    current_page_executor: int
+
+
+class ScrollingEXecutorsCallback(CallbackData, prefix="scorolling_executors"):
+    """Каллбэк для сценария пролистывания исполнителей."""
+
+    current_page: int
