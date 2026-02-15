141c946f95acae152efaa8e613233c5108b4c60d
raziel <tvindzon@gmail.com>
2026-02-15 15:06:51 +0700

    refactor: src\app\bot\core\bot.py

--- src/app/bot/core/bot.py
+++ src/app/bot/core/bot.py
@@ -1,19 +1,18 @@
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram import Bot, Dispatcher
 
-from core.logging.api import get_loggers
 
-
-def create_bot(bot_settings, proxy_settings):
-    logging_data = get_loggers(name=bot_settings.NAME_FOR_LOG_FOLDER)
+def create_bot(bot_settings, proxy_settings, logging_data):
 
     if proxy_settings.USE_PROXY:
         session: AiohttpSession = AiohttpSession(proxy=proxy_settings.get_http_url())
 
-        logging_data.info_logger.info(msg="Бот запущен с использванием прокси")
+        logging_data.info_logger.info(
+            msg="[USE PROXY] Прокси подключены"
+        )
         return Bot(token=bot_settings.TOKEN, session=session)
 
-    logging_data.info_logger.info("Бот запущен в обычном режиме")
+    logging_data.info_logger.info("[NOT USED PROXY] Прокси не были использованы")
     return Bot(token=bot_settings.TOKEN)
 
 
