a235d73aff34a7b4197bfda42faf5008f7b300ef
raziel <tvindzon@gmail.com>
2026-02-15 15:09:45 +0700

    refactor: src\app\bot\modules\admin\childes\add_executor\services\add_executor.py

--- src/app/bot/modules/admin/childes/add_executor/services/add_executor.py
+++ src/app/bot/modules/admin/childes/add_executor/services/add_executor.py
@@ -8,12 +8,16 @@ from app.bot.settings import settings
 from app.bot.db.uow import UnitOfWork
 from app.bot.view_model import SongResponse
 from core.error_handlers.helpers import ok, fail
-from core.error_handlers.format import format_errors_message
 from core.response.response_data import LoggingData, Result
-from core.response.messages import messages
+from core.error_handlers.decorator import safe_async_execution
+from app.bot.response import ServerDatabaseResponse
 
 
 class AddExecutorService:
+    @safe_async_execution(
+        message=ServerDatabaseResponse.ERROR_ADD_EXECUTOR.value,
+        code=ServerDatabaseResponse.ERROR_ADD_EXECUTOR.name,
+    )
     async def import_executor_from_path(
         self,
         executor_name: str,
@@ -38,95 +42,85 @@ class AddExecutorService:
 
         Не содержит логики взаимодействия с Telegram UI.
         """
-        try:
-            albums = add_executor_api.parse_album(base_path=base_path)
-            for parsed_album in albums:
-                cancel: bool = await update_progress()
-                if not cancel:
-                    await uow.session.rollback()
-                    return fail(
-                        code="CANCEL_OPERATION",
-                        message=messages.CANCEL_MESSAGE,
-                    )
-                async with UnitOfWork() as uow:
-                    genres_list_executor: List[
-                        str
-                    ] = await uow.genres.get_or_create_genres(titles=genres)
-                    executor = (
-                        await uow.executors.get_base_executor_by_name_and_country(
-                            name=executor_name, country=country
-                        )
-                    )
-                    if not executor:
-                        executor = await uow.executors.create_base_executor(
-                            name=executor_name,
-                            genres=genres_list_executor,
-                            country=country,
-                            file_id=file_id,
-                            file_unique_id=file_unique_id,
-                        )
-                    executor_id = executor.id
-
-                    array_songs: List = []
-                    exists_album = await uow.albums.get_album_by_title(
-                        executor_id=executor_id,
-                        title=parsed_album.title,
-                    )
-                    if exists_album:
-                        continue
-
-                    album = await uow.albums.create_album(
-                        executor=executor,
-                        title=parsed_album.title,
-                        year=parsed_album.year,
-                        photo_file_unique_id=settings.ALBUM_DEFAULT_PHOTO_UNIQUE_ID,
-                        photo_file_id=settings.ALBUM_DEFAULT_PHOTO_FILE_ID,
-                    )
-
-                    position: int = 0
-                    for audio_path in parsed_album.path.iterdir():
-
-                        if audio_path.suffix.lower() not in audio_extensions:
-                            continue
-                        try:
-
-                            msg: Message = await get_audio_telegram(
-                                audio_path,
-                                parsed_album.title,
-                            )
-                            position += 1
-                            array_songs.append(
-                                SongResponse(
-                                    file_id=msg.audio.file_id,
-                                    file_unique_id=msg.audio.file_unique_id,
-                                    position=position,
-                                    title=audio_path.stem.lower(),
-                                )
-                            )
-                        except Exception as err:
-                            logging_data.warning_logger.warning(
-                                "Не удалось загрузить трек",
-                                extra={"path": audio_path, "error": str(err)},
-                            )
-
-                    if array_songs:
-                        await uow.songs.create_songs(
-                            song_repsonse=array_songs,
-                            album=album,
-                        )
-            return ok(data=f"{executor_name}: {country} с альбомами был создан")
-        except Exception as err:
-            logging_data.error_logger.exception(
-                msg=format_errors_message(
-                    name_router=logging_data.router_name,
-                    function_name=self.import_executor_from_path.__name__,
-                    error_text=str(err),
+        albums = add_executor_api.parse_album(base_path=base_path)
+        for parsed_album in albums:
+            cancel: bool = await update_progress()
+            if not cancel:
+                return fail(
+                    code=ServerDatabaseResponse.CANCEL_OPERATION.name,
+                    message=ServerDatabaseResponse.CANCEL_OPERATION.value,
                 )
+
+            async with UnitOfWork() as uow:
+                genres_list_executor: List[str] = await uow.genres.get_or_create_genres(
+                    titles=genres
+                )
+                executor = await uow.executors.get_base_executor_by_name_and_country(
+                    name=executor_name, country=country
+                )
+                if not executor:
+                    executor = await uow.executors.create_base_executor(
+                        name=executor_name,
+                        genres=genres_list_executor,
+                        country=country,
+                        file_id=file_id,
+                        file_unique_id=file_unique_id,
+                    )
+                executor_id = executor.id
+
+                array_songs: List = []
+                exists_album = await uow.albums.get_album_by_title(
+                    executor_id=executor_id,
+                    title=parsed_album.title,
+                )
+                if exists_album:
+                    continue
+
+                album = await uow.albums.create_album(
+                    executor=executor,
+                    title=parsed_album.title,
+                    year=parsed_album.year,
+                    photo_file_unique_id=settings.ALBUM_DEFAULT_PHOTO_UNIQUE_ID,
+                    photo_file_id=settings.ALBUM_DEFAULT_PHOTO_FILE_ID,
+                )
+
+                position: int = 0
+                for audio_path in parsed_album.path.iterdir():
+
+                    if audio_path.suffix.lower() not in audio_extensions:
+                        continue
+                    try:
+
+                        msg: Message = await get_audio_telegram(
+                            audio_path,
+                            parsed_album.title,
+                        )
+                        position += 1
+                        array_songs.append(
+                            SongResponse(
+                                file_id=msg.audio.file_id,
+                                file_unique_id=msg.audio.file_unique_id,
+                                position=position,
+                                title=audio_path.stem.lower(),
+                            )
+                        )
+                    except Exception as err:
+                        logging_data.warning_logger.warning(
+                            "Не удалось загрузить трек",
+                            extra={"path": audio_path, "error": str(err)},
+                        )
+
+                if array_songs:
+                    await uow.songs.create_songs(
+                        song_repsonse=array_songs,
+                        album=album,
+                    )
+        return ok(
+            data=ServerDatabaseResponse.SUCCESS_ADD_EXECUTOR.value.format(
+                name=executor_name,
+                country=country,
             )
-            return fail(
-                code="ADD_EXECUTOR_ERROR",
-                message=messages.SERVER_ERROR,
-            )
+        )
 
 
 add_executor_service: AddExecutorService = AddExecutorService()
