71fcabf080fdb2c8070888d8f3a97ee3e4e8e2db
raziel <tvindzon@gmail.com>
2026-02-19 15:24:00 +0700

    feat: src\app\bot\modules\admin\childes\add_executor\services\add_executor.py

--- src/app/bot/modules/admin/childes/add_executor/services/add_executor.py
+++ src/app/bot/modules/admin/childes/add_executor/services/add_executor.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from aiogram.types import Message
 
 from app.bot.modules.admin.childes.add_executor.api.add_executor import add_executor_api
+from app.bot.modules.admin.childes.add_executor.settings import settings
 from app.bot.settings import settings
 from app.bot.db.uow import UnitOfWork
 from app.bot.view_model import SongResponse
@@ -11,9 +12,13 @@ from core.error_handlers.helpers import ok, fail
 from core.response.response_data import LoggingData, Result
 from core.error_handlers.decorator import safe_async_execution
 from app.bot.response import ServerDatabaseResponse
+from core.logging.api import get_loggers
 
 
 class AddExecutorService:
+    def __init__(self, logging_data: LoggingData):
+        self.logging_data: LoggingData = logging_data
+
     @safe_async_execution(
         message=ServerDatabaseResponse.ERROR_ADD_EXECUTOR.value,
         code=ServerDatabaseResponse.ERROR_ADD_EXECUTOR.name,
@@ -27,7 +32,6 @@ class AddExecutorService:
         file_id: str,
         file_unique_id: str,
         get_audio_telegram: Callable[[Path, str], Awaitable[Message]],
-        logging_data: LoggingData,
         audio_extensions: Set[str],
         update_progress: Callable[[], Awaitable[bool]],
     ) -> Result:
@@ -84,7 +88,6 @@ class AddExecutorService:
                     photo_file_id=settings.ALBUM_DEFAULT_PHOTO_FILE_ID,
                 )
 
-                position: int = 0
                 for audio_path in parsed_album.path.iterdir():
 
                     if audio_path.suffix.lower() not in audio_extensions:
@@ -95,17 +98,15 @@ class AddExecutorService:
                             audio_path,
                             parsed_album.title,
                         )
-                        position += 1
                         array_songs.append(
                             SongResponse(
                                 file_id=msg.audio.file_id,
                                 file_unique_id=msg.audio.file_unique_id,
-                                position=position,
                                 title=audio_path.stem.lower(),
                             )
                         )
                     except Exception as err:
-                        logging_data.warning_logger.warning(
+                        self.logging_data.warning_logger.warning(
                             "Не удалось загрузить трек",
                             extra={"path": audio_path, "error": str(err)},
                         )
@@ -113,7 +114,7 @@ class AddExecutorService:
                 if array_songs:
                     await uow.songs.create_songs(
                         song_repsonse=array_songs,
-                        album=album,
+                        album_id=album.id,
                     )
         return ok(
             data=ServerDatabaseResponse.SUCCESS_ADD_EXECUTOR.value.format(
@@ -122,5 +123,36 @@ class AddExecutorService:
             )
         )
 
+    async def chek_executor_exists(
+        self,
+        country: str,
+        executor_name: str,
+    ) -> Result:
+        """
+        Application service для сценария провекри существования в базе данных.
 
-add_executor_service: AddExecutorService = AddExecutorService()
+        Отвечает за:
+        - оркестрацию вызова AddExecutorAPI
+        - обработку ошибок
+        - работу с базой данных
+        - подготовку данных для handlers
+
+        Не содержит логики взаимодействия с Telegram UI.
+        """
+
+        executor_exists = None
+        async with UnitOfWork() as uow:
+            executor = await uow.executors.get_base_executor_by_name_and_country(
+                country=country, name=executor_name
+            )
+            if executor:
+                genres: List[str] = [genre.title for genre in executor.genres]
+                executor_exists = True
+        if executor_exists:
+            return ok(data=genres)
+        return fail(code="NOT_EXECUTOR_EXISTS", message="Исполнителя нет в базе данных")
+
+
+add_executor_service: AddExecutorService = AddExecutorService(
+    logging_data=get_loggers(name=settings.NAME_FOR_LOG_FOLDER)
+)
