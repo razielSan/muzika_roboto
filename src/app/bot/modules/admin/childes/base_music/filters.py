0663c693787e4e6a06f13717ecb457e51a9c33da
raziel <tvindzon@gmail.com>
2026-02-19 15:25:19 +0700

    feat: src\app\bot\modules\admin\childes\base_music\filters.py

--- src/app/bot/modules/admin/childes/base_music/filters.py
+++ src/app/bot/modules/admin/childes/base_music/filters.py
@@ -11,9 +11,11 @@ class AdminBackExecutorCallback(CallbackData, prefix="admin_back_executor"):
 
 
 class AdminMusicCallback(CallbackData, prefix="admin_base_music"):
+    """Каллбэк для сценаря показа альбома со списокм песен"""
     executor_id: int
     album_id: int
     current_page_executor: int
+    count_pages_executor: int
 
 
 class AdminPlaySongCallback(CallbackData, prefix="admin_play_song"):
@@ -26,7 +28,7 @@ class AdminDeleteExecutorCallback(CallbackData, prefix="admin_del_executor"):
 
 
 class AdminConfirmDeleteExecutorCallback(
-    CallbackData, prefix="confirm_admin_del_executor"
+    CallbackData, prefix="admin_confirm_del_executor"
 ):
     executor_id: Optional[int]
 
@@ -34,17 +36,25 @@ class AdminConfirmDeleteExecutorCallback(
 class AdminDeleteAlbumCallback(CallbackData, prefix="admin_del_album"):
     executor_id: int
     album_id: int
+    current_page_executor: int
+    count_pages_executor: int
 
 
-class AdminConfirmDeleteAlbumCallback(CallbackData, prefix="confirm_admin_del_album"):
+class AdminConfirmDeleteAlbumCallback(CallbackData, prefix="admin_confirm_del_album"):
+    """Каллбэк для сценаря подтверждения или отмены удаления альбома"""
     executor_id: Optional[int]
     album_id: Optional[int]
+    current_page_executor: Optional[int]
+    count_pages_executor: Optional[int]
 
 
 class AdminDeleteSongMenuCallback(CallbackData, prefix="admin_del_song_menu"):
     """Каллбэк для сценрая показа инлайн меню выбора песен для удаления."""
 
     album_id: int
+    executor_id: int
+    current_page_executor: int
+    count_pages_executor: int
 
 
 class AdminButtonDeleteSongCallback(CallbackData, prefix="admin_button_del_song"):
@@ -53,22 +63,24 @@ class AdminButtonDeleteSongCallback(CallbackData, prefix="admin_button_del_song"
     position: int
 
 
-class AdminConfirmDeleteSongCallback(CallbackData, prefix="confirm_admin_del_songs"):
+class AdminConfirmDeleteSongCallback(CallbackData, prefix="admin_confirm_del_songs"):
+    """Каллбэк для сценаря подтверждения или отмены удаления песен"""
     album_id: int
 
 
-class AdminCancelDeleteSongCallback(CallbackData, prefix="cancel_admin_del_songs"):
+class AdminCancelDeleteSongCallback(CallbackData, prefix="admin_cancel_del_songs"):
     """Каллбэк для сценария отмены удаления песен."""
-    pass
-
-
-class AdminCompleteDeleteSongCallback(CallbackData, prefix="compl_admin_del_songs"):
-    """Каллбэк для сценрая удаления песен после подтверждения."""
 
     pass
 
 
-class AdminScrollingSongsCallback(CallbackData, prefix="scrolling_admin_songs"):
+class AdminCompleteDeleteSongCallback(CallbackData, prefix="admin_compl_del_songs"):
+    """Каллбэк для сценапмя удаления песен после подтверждения."""
+
+    pass
+
+
+class AdminScrollingSongsCallback(CallbackData, prefix="admin_scrolling_songs"):
     """Каллбэк для сценария пролистывания песен в альбоме."""
 
     executor_id: int
@@ -76,9 +88,12 @@ class AdminScrollingSongsCallback(CallbackData, prefix="scrolling_admin_songs"):
     position: int
     offset: int
     current_page_executor: int
-    
+    count_pages_executor: int
 
-class AdminScrollingSongsMenuDeleteCallback(CallbackData, prefix="scrolling_del_admin_songs"):
+
+class AdminScrollingSongsMenuDeleteCallback(
+    CallbackData, prefix="admin_scrolling_del_songs"
+):
     """Каллбэк для сценария пролистывания песен в меню удаления песен."""
 
     album_id: int
@@ -86,7 +101,7 @@ class AdminScrollingSongsMenuDeleteCallback(CallbackData, prefix="scrolling_del_
     offset: int
 
 
-class AdminScrollingAlbumsCallback(CallbackData, prefix="scrolling_admin_albums"):
+class AdminScrollingAlbumsCallback(CallbackData, prefix="admin_scrolling_albums"):
     """Каллбэк для сценария пролистывания альбомов."""
 
     executor_id: int
@@ -107,36 +122,61 @@ class AdminUpdatePhotoExecutorCallback(
     CallbackData, prefix="update_admin_executor_photo"
 ):
     executor_id: int
+    current_page_executor: int
+    count_pages_executor: int
 
 
-class AdminUpdatePhotoAlbumCallback(CallbackData, prefix="update_admin_album_photo"):
+class AdminUpdatePhotoAlbumCallback(CallbackData, prefix="admin_update_album_photo"):
     executor_id: int
     album_id: int
+    current_page_executor: int
+    count_pages_executor: int
 
 
 class AdminUpdateExecutorNameCallback(
-    CallbackData, prefix="update_admin_executor_name"
+    CallbackData, prefix="admin_update_executor_name"
 ):
     executor_id: int
 
 
 class AdminUpdateExecutorCountryCallback(
-    CallbackData, prefix="update_admin_executor_country"
+    CallbackData, prefix="admin_update_executor_country"
 ):
     executor_id: int
+    current_page_executor: int
+    count_pages_executor: int
 
 
-class AdminUpdateAlbumTitleCallback(CallbackData, prefix="update_admin_album_title"):
+class AdminUpdateAlbumTitleCallback(CallbackData, prefix="admin_update_album_title"):
     executor_id: int
     album_id: int
+    current_page_executor: int
+    count_pages_executor: int
 
 
-class AdminUpdateAlbumYearCallback(CallbackData, prefix="update_admin_album_year"):
+class AdminUpdateAlbumYearCallback(CallbackData, prefix="admin_update_album_year"):
     executor_id: int
     album_id: int
+    current_page_executor: int
+    count_pages_executor: int
 
 
 class AdminUpdateExecutorGenresCallback(
-    CallbackData, prefix="update_admin_executor_genres"
+    CallbackData, prefix="admin_update_executor_genres"
 ):
     executor_id: int
+    current_page_executor: int
+    count_pages_executor: int
+
+
+class AdminAddSongsCallback(CallbackData, prefix="admin_add_songs"):
+    executor_id: int
+    album_id: int
+    current_page_executor: int
+    count_pages_executor: int
+
+
+class AdminAddAlbumCallback(CallbackData, prefix="admin_add_album"):
+    executor_id: int
+    current_page_executor: int
+    count_pages_executor: int
