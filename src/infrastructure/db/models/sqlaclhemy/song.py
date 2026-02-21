33028b391d3c59488767850ea3b1cf0c3681db79
raziel <tvindzon@gmail.com>
2026-02-21 15:37:21 +0700

    create: src\infrastructure\db\models\sqlaclhemy\song.py

---
+++ src/infrastructure/db/models/sqlaclhemy/song.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy import CheckConstraint
+
+from infrastructure.db.models.sqlaclhemy.base import Base
+
+
+class Song(Base):
+    __tablename__ = "song"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    position: Mapped[int] = mapped_column(
+        CheckConstraint(
+            "position > 0",
+            name="position_positive",
+        )
+    )
+    file_id: Mapped[str]
+    file_unique_id: Mapped[str]
+
+    album_id: Mapped[int] = mapped_column(ForeignKey("album.id", ondelete="CASCADE"))
+    album: Mapped["Album"] = relationship(back_populates="songs")
+
+    __table_args__ = (UniqueConstraint("album_id", "position"),)
