5a1102c6588d06154cbb7d5c72701baaee7a3851
raziel <tvindzon@gmail.com>
2026-02-21 15:38:01 +0700

    create: __init__.py

---
+++ src/infrastructure/db/models/sqlaclhemy/__init__.py
@@ -0,0 +1,10 @@
+from infrastructure.db.models.sqlaclhemy.base import Base
+from infrastructure.db.models.sqlaclhemy.album import Album
+from infrastructure.db.models.sqlaclhemy.song import Song
+from infrastructure.db.models.sqlaclhemy.user_genre_executor import (
+    User,
+    Executor,
+    Genre,
+    GenreExecutor,
+    UserExecutor,
+)
