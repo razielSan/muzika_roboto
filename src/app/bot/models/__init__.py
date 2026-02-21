99f44f8aa79fffa896466e4b927a95eff065ceb7
raziel <tvindzon@gmail.com>
2026-02-21 15:38:27 +0700

    delete: src\app\bot\models

--- src/app/bot/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.bot.models.base import Base
-from app.bot.models.genre_executor import GenreExecutor, Genre, Executor
-from app.bot.models.album import Album
-from app.bot.models.song import Song
-from app.bot.models.user import User
