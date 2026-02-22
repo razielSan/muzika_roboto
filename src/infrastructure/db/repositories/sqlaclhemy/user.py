b68bbae34f18f59f637cc17577ccc8f7e429cbec
raziel <tvindzon@gmail.com>
2026-02-22 15:33:15 +0700

    create: src\infrastructure\db\repositories\sqlaclhemy\user.py

---
+++ src/infrastructure/db/repositories/sqlaclhemy/user.py
@@ -0,0 +1,30 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from typing import Optional
+
+from infrastructure.db.models.sqlaclhemy import User
+
+
+class SQLAlchemyUserRepository:
+    model = User
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create_user(self, name: str, telegram: int) -> User:
+
+        user = self.model(
+            name=name,
+            telegram=telegram,
+        )
+
+        self.session.add(user)
+        await self.session.flush()
+        return user
+
+    async def get_user_by_telegram(self, telegram: int) -> Optional[User]:
+
+        user = await self.session.scalar(
+            select(self.model).where(self.model.telegram == telegram)
+        )
+        return user
