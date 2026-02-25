98bb8e7d62acb77968f66ab0cdae02e76246d3e5
raziel <tvindzon@gmail.com>
2026-02-25 13:22:47 +0700

    refactor: src\domain\entities\db\models\collection_song.py

--- src/domain/entities/db/models/collection_song.py
+++ src/domain/entities/db/models/collection_song.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
-from typing import Optional
 
 
 @dataclass
 class CollectionSong:
     id: int
     title: str
-    position: str
+    position: int
+    photo_file_id: str
+    photo_file_unique_id: str
     file_id: str
     file_unique_id: str
-    user_id: Optional[int]
+    user_id: int
