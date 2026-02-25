c357771bc87e5e1802436a3e33076ca78b3a70a7
raziel <tvindzon@gmail.com>
2026-02-25 13:28:02 +0700

    create: src\infrastructure\aiogram\keyboards\utils.py

---
+++ src/infrastructure/aiogram/keyboards/utils.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def build_pages(current: int, total: int) -> List[int]:
+    pages = {1, total}
+    middle_page = total // 2
+
+    pages.add(current)
+    pages.add(middle_page)
+
+    if current - 1 > 1:
+        pages.add(current - 1)
+
+    if current + 1 < total:
+        pages.add(current + 1)
+
+    if current + 2 < total:
+        pages.add(current + 2)
+
+    if current + 2 < total:
+        pages.add(current + 3)
+
+    return sorted(pages)
