562c474f2d5f568636f6d38a9f6df7ce6cfaf8ae
raziel <tvindzon@gmail.com>
2026-02-25 13:25:09 +0700

    feat: src\domain\errors\error_code.py

--- src/domain/errors/error_code.py
+++ src/domain/errors/error_code.py
@@ -5,5 +5,9 @@ class ErorrCode(Enum):
     UNKNOWN_ERROR: str = "UNKNOWN_ERROR"
 
 
-class ExistsErrorCode(Enum):
+class ExistsCode(Enum):
     USER_ALREADY_EXISTS: str = "USER_ALREADY_EXISTS"
+
+
+class NotFoundCode(Enum):
+    USER_NOT_FOUND: str = "USER_NOT_FOUND"
