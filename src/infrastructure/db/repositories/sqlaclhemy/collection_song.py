793d25348325818e209d1cc0876057acdca04017
raziel <tvindzon@gmail.com>
2026-02-27 14:46:44 +0700

    refactor: src\infrastructure\db\repositories\sqlaclhemy\collection_song.py

--- src/infrastructure/db/repositories/sqlaclhemy/collection_song.py
+++ src/infrastructure/db/repositories/sqlaclhemy/collection_song.py
@@ -1,7 +1,9 @@
+from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, func
 
 from infrastructure.db.models.sqlaclhemy.collection_song import CollectionSong
+from domain.entities.response import CollectionSongResponse
 
 
 class SQLAlchemyCollectionSongRepository:
@@ -17,7 +19,38 @@ class SQLAlchemyCollectionSongRepository:
         stmt = await self.session.scalars(
             select(self.model).where(
                 self.model.user_id == user_id,
-            )
+            ).order_by(self.model.position)
         )
         collection_songs = stmt.all()
         return collection_songs
+
+    async def get_last_poistion_song(self, user_id: int):
+        stmt = await self.session.scalars(
+            select(func.max(self.model.position)).where(
+                self.model.user_id == user_id,
+            )
+        )
+        max_position = stmt.first()
+
+        return max_position
+
+    async def add_songs(
+        self,
+        collection_songs: List[CollectionSongResponse],
+        user_id: int,
+        start_position: int = 1,
+    ):
+        list_songs = []
+        for position, song in enumerate(collection_songs, start=start_position):
+            list_songs.append(
+                self.model(
+                    title=song.title,
+                    file_id=song.file_id,
+                    position=position,
+                    user_id=user_id,
+                    file_unique_id=song.file_unique_id,
+                )
+            )
+        self.session.add_all(list_songs)
+        await self.session.flush()
+        return list_songs
