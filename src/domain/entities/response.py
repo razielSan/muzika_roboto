554c4d21e045fca9fe9caf87b1e8079ccc10fc27
raziel <tvindzon@gmail.com>
2026-02-27 14:40:34 +0700

    feat:: src\domain\entities\response.py

--- src/domain/entities/response.py
+++ src/domain/entities/response.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional, List
 
 
 @dataclass
@@ -6,3 +7,11 @@ class CollectionSongResponse:
     file_id: str
     file_unique_id: str
     title: str
+    position: Optional[int] = None
+
+
+@dataclass
+class UserCollectionSongResponse:
+    collection_songs: List[CollectionSongResponse]
+    collection_song_photo_unique_id: Optional[str] = None
+    collection_song_photo_file_id: Optional[str] = None
