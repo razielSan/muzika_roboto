db4eb1e74a1ecfd263a5f32cd0532c8b6d08ac6d
raziel <tvindzon@gmail.com>
2026-02-27 14:45:15 +0700

    feat: src\domain\errors\error_code.py

--- src/domain/errors/error_code.py
+++ src/domain/errors/error_code.py
@@ -3,11 +3,12 @@ from enum import Enum
 
 class ErorrCode(Enum):
     UNKNOWN_ERROR: str = "UNKNOWN_ERROR"
-
-
-class ExistsCode(Enum):
     USER_ALREADY_EXISTS: str = "USER_ALREADY_EXISTS"
 
 
 class NotFoundCode(Enum):
     USER_NOT_FOUND: str = "USER_NOT_FOUND"
+    
+    
+class SuccessCode(Enum):
+    SUCCESS_COLLECTION_SONG: str = "SUCCESS_COLLECTION_SONG"
