7edb652941f61ce56bc87cb4b872cf7e0349d53c
raziel <tvindzon@gmail.com>
2026-02-27 14:36:41 +0700

    refactor: src\app\bot\modules\music_library\childes\collection_of_songs\settings.py

--- src/app/bot/modules/music_library/childes/collection_of_songs/settings.py
+++ src/app/bot/modules/music_library/childes/collection_of_songs/settings.py
@@ -3,13 +3,13 @@ from pydantic import BaseModel
 
 class ModuleSettings(BaseModel):
     SERVICE_NAME: str = "music_library.childes.collection_of_songs"
-    MENU_REPLY_TEXT: str = "music_library.childes.collection_of_songs" 
+    MENU_REPLY_TEXT: str = "music_library.childes.collection_of_songs"
     MENU_CALLBACK_TEXT: str = "🎧 Мой Сборник Песен"
     MENU_CALLBACK_DATA: str = "music_library.childes.collection_of_songs"
     SHOW_IN_MAIN_MENU: bool = True
     NAME_FOR_LOG_FOLDER: str = "music_library"
     NAME_FOR_TEMP_FOLDER: str = "music_library/childes/collection_of_songs"
     ROOT_PACKAGE: str = "app.bot.modules.music_library.childes.collection_of_songs"
-    
+
+
 settings: ModuleSettings = ModuleSettings()
-    
