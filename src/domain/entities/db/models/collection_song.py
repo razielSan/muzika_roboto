ca2a0031309153cf8f2ea9e2831de75a4eb6418f
raziel <tvindzon@gmail.com>
2026-02-27 14:40:48 +0700

    refactor: src\domain\entities\db\models\collection_song.py

--- src/domain/entities/db/models/collection_song.py
+++ src/domain/entities/db/models/collection_song.py
@@ -6,8 +6,6 @@ class CollectionSong:
     id: int
     title: str
     position: int
-    photo_file_id: str
-    photo_file_unique_id: str
     file_id: str
     file_unique_id: str
     user_id: int
