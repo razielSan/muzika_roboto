c1198f15f2313082716a8b94ede6d2489650d409
raziel <tvindzon@gmail.com>
2026-02-27 14:45:03 +0700

    feat: src\domain\entities\db\repositories\collection_song.py

--- src/domain/entities/db/repositories/collection_song.py
+++ src/domain/entities/db/repositories/collection_song.py
@@ -1,7 +1,8 @@
-from typing import Sequence
+from typing import Sequence, Optional, List
 from abc import ABC, abstractmethod
 
 from domain.entities.db.models.collection_song import CollectionSong
+from domain.entities.response import CollectionSongResponse
 
 
 class CollectionSongRepository(ABC):
@@ -10,4 +11,17 @@ class CollectionSongRepository(ABC):
         self,
         user_id: int,
     ) -> Sequence[CollectionSong]:
+        pass
+
+    @abstractmethod
+    async def get_last_poistion_song(self, user_id: int) -> Optional[int]:
+        pass
+
+    @abstractmethod
+    async def add_songs(
+        self,
+        collection_songs: List[CollectionSongResponse],
+        user_id: int,
+        start_position: int = 1,
+    ) -> List:
         pass
