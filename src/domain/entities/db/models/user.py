2e04bf394aeaaa33c5b0a937aee67537265236e0
raziel <tvindzon@gmail.com>
2026-02-27 14:44:03 +0700

    refactor: src\domain\entities\db\models\user.py

--- src/domain/entities/db/models/user.py
+++ src/domain/entities/db/models/user.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
+from typing import Optional
+
 
 @dataclass
 class User:
     id: int
     name: str
     telegram: int
+    collection_song_photo_file_id: Optional[str] = None
+    collection_song_photo_unique_id: Optional[str] = None
