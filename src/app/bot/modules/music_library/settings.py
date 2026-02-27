3c80329fc8139c70f596cd429e2ae035fe74fb0d
raziel <tvindzon@gmail.com>
2026-02-27 14:36:26 +0700

    refactor: src\app\bot\modules\music_library\settings.py

--- src/app/bot/modules/music_library/settings.py
+++ src/app/bot/modules/music_library/settings.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class ModuleSettings(BaseModel):
     SERVICE_NAME: str = "music_library"
     MENU_REPLY_TEXT: str = "music_library"
-    MENU_CALLBACK_TEXT: str = "music_library"
+    MENU_CALLBACK_TEXT: str = "📻 Моя Музыкальная Коллекция"
     MENU_CALLBACK_DATA: str = "music_library"
     SHOW_IN_MAIN_MENU: bool = True
     NAME_FOR_LOG_FOLDER: str = "music_library"
