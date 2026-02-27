c5252f97b589a0cd1a7604909462dc99d66b1322
raziel <tvindzon@gmail.com>
2026-02-27 14:45:27 +0700

    feat: src\infrastructure\aiogram\filters.py

--- src/infrastructure/aiogram/filters.py
+++ src/infrastructure/aiogram/filters.py
@@ -4,3 +4,9 @@ from aiogram.filters.callback_data import CallbackData
 class AddCallbackDataFilters:
     class CollectionSong(CallbackData, prefix="add_collection_song"):
         pass
+
+
+class ScrollingCallbackDataFilters:
+    class CollectionSong(CallbackData, prefix="scrolling_collection_song"):
+        position: int
+        offset: int
