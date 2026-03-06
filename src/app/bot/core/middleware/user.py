3d2294fe78655a67eaf66551e80118e2d0607773
raziel <tvindzon@gmail.com>
2026-03-06 13:42:41 +0700

    fefactor: src\app\bot\core\middleware\user.py

--- src/app/bot/core/middleware/user.py
+++ src/app/bot/core/middleware/user.py
@@ -1,21 +1,37 @@
 from typing import Optional, Any
 
 from aiogram import BaseMiddleware
+from aiogram.fsm.context import FSMContext
 
+from domain.errors.error_code import NotFoundCode
+from domain.entities.db.models.user import User as UserDomain
 from infrastructure.db.uow import UnitOfWork
+from infrastructure.aiogram.messages import resolve_message
 from infrastructure.db.mappers.user_mapper import to_domain_user
 
 
 class UserMiddleware(BaseMiddleware):
     """Middleware для возврата user."""
 
-    async def __call__(self, handler, event, data) -> Optional[Any]:
-        """Вовзращаем user."""
-        telegram_id = event.from_user.id
-        response_user = None
+    async def __call__(
+        self,
+        handler,
+        event,
+        data,
+    ) -> Optional[Any]:
+        """Вовзращает user."""
+        telegram_id: int = event.from_user.id
+
         async with UnitOfWork() as uwo:
-            user = await uwo.users.get_user_by_telegram(telegram=telegram_id)
-            if user:
-                response_user: str = to_domain_user(user)
+            user: UserDomain = await uwo.users.get_user_by_telegram(telegram=telegram_id)
+            if not user:  # если пользователь не найден
+                state: FSMContext = data.get("state")
+                await state.clear()
+                error_message: str = resolve_message(code=NotFoundCode.USER_NOT_FOUND.name)
+                if hasattr(event, "message"):  # если каллбэк событие
+                    return await event.message.answer(text=error_message)
+                else:
+                    return await event.answer(text=error_message)
+            response_user: UserDomain = to_domain_user(model=user)
         data["user"] = response_user
         return await handler(event, data)
