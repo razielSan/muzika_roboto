91ce845fe53ca5c2c8324c272a03497b22c45380
raziel <tvindzon@gmail.com>
2026-03-06 14:00:46 +0700

    refactor: src\application\use_cases\db\user\update_user_collection_songs_photo_file_id.py

--- src/application/use_cases/db/user/update_user_collection_songs_photo_file_id.py
+++ src/application/use_cases/db/user/update_user_collection_songs_photo_file_id.py
@@ -1,7 +1,7 @@
 from domain.entities.db.uow import AbstractUnitOfWork
 from domain.errors.error_code import ErorrCode, NotFoundCode, SuccessCode
 from core.error_handlers.decorator import safe_async_execution
-from core.error_handlers.helpers import ok, fail
+from core.error_handlers.helpers import ok
 from core.response.response_data import Result
 
 
@@ -15,25 +15,14 @@ class UpdateUserCollectionSongsPhotoFileId:
     )
     async def execute(
         self,
-        telegram: int,
         photo_file_id: str,
         photo_file_unique_id: str,
+        user_id: int,
     ) -> Result:
-        user_id = None
         async with self.uow as uow:
-            user = await uow.users.get_user_by_telegram(
-                telegram=telegram,
-            )
-            if not user:
-                return fail(
-                    code=NotFoundCode.USER_NOT_FOUND.name,
-                    message=NotFoundCode.USER_NOT_FOUND.value,
-                )
-
             await uow.users.update_collection_songs_photo_file_id(
-                user_id=user.id,
+                user_id=user_id,
                 photo_file_id=photo_file_id,
                 photo_file_unique_id=photo_file_unique_id,
             )
-            user_id: int = user.id
         return ok(data=user_id, code=SuccessCode.UPDATE_PHOTO_SUCCESS.name)
