c738aed014bb5ceb060430431b4f61e390de7878
raziel <tvindzon@gmail.com>
2026-03-09 15:42:08 +0700

    feat: src\infrastructure\db\utils\editing.py

--- src/infrastructure/db/utils/editing.py
+++ src/infrastructure/db/utils/editing.py
@@ -5,14 +5,26 @@ def get_information_executor(
     name: str,
     country: str,
     genres: List[str],
-    len_albums: int,
+    number_of_albums: int,
 ) -> str:
     genre_str: str = ", ".join(genres).strip()
     data: str = (
         "🎸 Исполнитель: "
         f"{name}\n🌏 Страна: {country}\n\n"
         f"🎹 Жанры: {genre_str}\n"
-        f"🎺 Количество альбомов: {len_albums}"
+        f"🎺 Количество альбомов: {number_of_albums}"
     )
 
     return data
+
+
+def get_information_album(
+    title: str,
+    year: int,
+    number_of_songs: int
+):
+    return (
+        f"🎸 Год: {year}\n"
+        f"🎸 Название: {title}\n"
+        f"🎸 Количестов песен: {number_of_songs}"
+    )
