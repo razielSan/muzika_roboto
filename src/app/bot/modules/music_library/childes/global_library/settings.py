79ffa4c259eff276782c1abd2fca148faac9ae47
raziel <tvindzon@gmail.com>
2026-03-13 15:59:19 +0700

    refactor: src\app\bot\modules\music_library\childes\global_library\settings.py

--- src/app/bot/modules/music_library/childes/global_library/settings.py
+++ src/app/bot/modules/music_library/childes/global_library/settings.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class ModuleSettings(BaseModel):
     SERVICE_NAME: str = "music_library.childes.global_library"
     MENU_REPLY_TEXT: str = "music_library.childes.global_library" 
-    MENU_CALLBACK_TEXT: str = "🎹 Глобальная Музыкальная Коллекция"
+    MENU_CALLBACK_TEXT: str = "🎹 Глобальная Библиотека"
     MENU_CALLBACK_DATA: str = "music_library.childes.global_library"
     SHOW_IN_MAIN_MENU: bool = True
     NAME_FOR_LOG_FOLDER: str = "music_library"
