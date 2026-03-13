a7b28ba8bfc83a08b80f6804962cb4766f0eefc9
raziel <tvindzon@gmail.com>
2026-03-13 19:52:18 +0700

    feat: src\domain\entities\db\repositories\user_executor.py

--- src/domain/entities/db/repositories/user_executor.py
+++ src/domain/entities/db/repositories/user_executor.py
@@ -11,3 +11,7 @@ class UserExecutorRepository(ABC):
     @abstractmethod
     async def exists(self, user_id: int, executor_id: int) -> bool:
         pass
+
+    @abstractmethod
+    async def delete(self, user_id: int, executor_id: int) -> bool:
+        pass
