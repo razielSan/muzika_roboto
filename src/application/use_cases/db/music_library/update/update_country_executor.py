fa1712f52f3d282d3812b92df555fda16dc6df6c
raziel <tvindzon@gmail.com>
2026-03-18 14:56:27 +0700

    create: src\application\use_cases\db\music_library\update\update_country_executor.py

---
+++ src/application/use_cases/db/music_library/update/update_country_executor.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+from domain.entities.db.uow import AbstractUnitOfWork
+from domain.errors.error_code import ErorrCode, SuccessCode, NotFoundCode
+from domain.entities.db.models.executor import Executor as ExecutorDomain
+from core.error_handlers.decorator import safe_async_execution
+from core.error_handlers.helpers import ok
+from core.response.response_data import Result, LoggingData
+
+
+class UpdateCountryExecutor:
+    def __init__(self, uow: AbstractUnitOfWork, logging_data: LoggingData):
+        self.uow: AbstractUnitOfWork = uow
+        self.logging_data: LoggingData = logging_data
+
+    @safe_async_execution(
+        message=ErorrCode.UNKNOWN_ERROR.value, code=ErorrCode.UNKNOWN_ERROR.name
+    )
+    async def execute(
+        self,
+        user_id: Optional[int],
+        executor_id: int,
+        country: str,
+    ) -> Result:
+        async with self.uow as uow:
+            executor: Optional[
+                ExecutorDomain
+            ] = await uow.executors.update_executor_country(
+                executoro_id=executor_id,
+                user_id=user_id,
+                country=country,
+            )
+            if not executor:
+                return ok(
+                    data=[], empty=True, code=NotFoundCode.EXECUTOR_NOT_FOUND.name
+                )
+
+        return ok(
+            data=SuccessCode.UPDATE_EXECUTOR_COUNTRY_SUCCESS.value,
+            code=SuccessCode.UPDATE_EXECUTOR_COUNTRY_SUCCESS.name,
+        )
