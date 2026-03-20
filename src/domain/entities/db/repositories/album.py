52de5705947417159129e32f27ab83cc53e7881c
raziel <tvindzon@gmail.com>
2026-03-20 19:43:26 +0700

    feat: src\domain\entities\db\repositories\album.py

--- src/domain/entities/db/repositories/album.py
+++ src/domain/entities/db/repositories/album.py
@@ -6,9 +6,28 @@ from domain.entities.db.models.album import Album as AlbumDomain
 
 class AlbumRepository(ABC):
     @abstractmethod
+    async def create_album(
+        self,
+        executor_id: int,
+        title: str,
+        year: int,
+        photo_file_id: str,
+        photo_file_unique_id: str,
+    ) -> AlbumDomain:
+        pass
+
+    @abstractmethod
     async def get_album(
         self,
         executor_id: int,
         album_id: int,
+    ) -> Optional[AlbumDomain]:
+        pass
+
+    @abstractmethod
+    async def get_album_by_title(
+        self,
+        executor_id: int,
+        title: str,
     ) -> Optional[AlbumDomain]:
         pass
