e4024fcfdf5eb04d8ec184533e13d70a3e827b47
raziel <tvindzon@gmail.com>
2026-03-20 19:44:11 +0700

    refactor: src\infrastructure\aiogram\messages.py

--- src/infrastructure/aiogram/messages.py
+++ src/infrastructure/aiogram/messages.py
@@ -6,6 +6,7 @@ ERRORS = {
     ErorrCode.USER_ALREADY_EXISTS.name: "⚠ Пользователь уже существует",
     ErorrCode.EXECUTOR_ALREADY_EXISTS.name: "⚠ Исполнитель уже существует",
     ErorrCode.USER_EXECUTOR_ALREADY_EXISTS.name: "⚠ Исполнитель присутствует в библиотеке",
+    ErorrCode.ALBUM_ALREADY_EXISTS.name: "⚠ Альбом уже существует",
 }
 
 
@@ -24,6 +25,7 @@ SUCCESS = {
     SuccessCode.ADD_EXECUTOR_SUCCESS.name: "✅ Исполнитель успешно создан",
     SuccessCode.ADD_SONGS_SUCCESS.name: "✅ Песни успешно добавлены",
     SuccessCode.ADD_USER_SUCCESS.name: "✅ Пользователь успешно добавлен",
+    SuccessCode.ADD_AlBUM_SUCCESS.name: "✅ Альбом успешно добавлен",
     SuccessCode.GET_SONGS_SUCCESS.name: "✅ Песни успешно получены",
     SuccessCode.GET_EXECUTORS_SUCCESS: "✅ Исполнители успешно получены",
     SuccessCode.GET_ALBUMS_SUCCESS.name: "✅ Альбомы успешно получены",
@@ -57,14 +59,15 @@ class UserMessages:
     CONFIRMATION_TEXT: str = "✅ Подтверждаю"
     CAPTION_DELETE_EXECUTOR: str = "❗️ Будет удален исполнитель\n\n❗️ {name}"
     CLICK_ONE_OF_THE_BUTTONS_ABOVE: str = "👆🏾 Нажмите одну из кнопок выше"
-    DROP_THE_SONG: str = "🎸 Скидывайте песни"
+    DROP_THE_SONG: str = "❗️ Скидывайте песни"
     DROP_THE_PHOTO: str = "❗️ Скидывайте фотографию"
     ENTER_THE_SONG_NAME: str = "❗️ Введите имя песни"
+    ENTER_THE_ALBUM_TITLE: str = "❗️ Введите заголовок альбома"
+    ENTER_THE_ALBUM_YEAR: str = "❗️ Введите год выхода альбома"
     ENTER_THE_СOUNTRY_EXECUTOR: str = "❗️ Введите страну исполнителя"
     ENTER_THE_PHOTO_DEFAULT: str = (
         "❗️ Скидывайте фотографию\n\n🖌 Любой символ: Дефолтная фотография"
     )
-    ENTER_THE_PHOTO: str = "❗️ Скидывайте фотографию"
     ENTER_THE_GENRES: str = (
         "❗️ Введите жанры исполнителя через точку\n\n"
         "🖌 Пример: панк-рок.метал.русский рок\n\n"
@@ -88,7 +91,7 @@ class UserMessages:
     THERE_ARE_NO_SONGS: str = "🤷🏻‍♀️ У вас нет песен в сборнике"
     THE_SONG_HAS_ALREADY_BEEN_ADDED: str = "🤷🏻‍♀️ Песня {title} уже была добавленна"
     THE_SONG_IS_SAVED: str = "🎼 Песня {title} была сохранена"
-    THE_DATA_MUST_BE_IN_THE_FORMAT: str = "🖊 Данные должны быть в формате {format}"
+    THE_DATA_MUST_BE_IN_THE_FORMAT: str = "🖊 Данные должны быть в формате\n\n{format}"
     USER_CANCEL_MESSAGE: str = "❌ Все запросы отменены"
     USER_PANEL_CAPTION: str = "🤔 Что же мне сегодня послушать ?"
 
