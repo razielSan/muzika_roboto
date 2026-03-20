1afa3a5a86e88c43ca11a9597cdbf2952fc6d6bf
raziel <tvindzon@gmail.com>
2026-03-20 19:43:37 +0700

    feat: src\domain\entities\db\repositories\song.py

--- src/domain/entities/db/repositories/song.py
+++ src/domain/entities/db/repositories/song.py
@@ -1,10 +1,24 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, List
 
 from domain.entities.db.models.song import Song as SongDomain
+from domain.entities.response import SongResponse
 
 
 class SongRepository(ABC):
     @abstractmethod
     async def get_song(self, album_id: int, song_id: int) -> Optional[SongDomain]:
+        pass
+
+    @abstractmethod
+    async def get_last_postion_song(self, album_id: int) -> Optional[int]:
+        pass
+
+    @abstractmethod
+    async def add_songs(
+        self,
+        songs: List[SongResponse],
+        album_id: int,
+        start_position: int = 1,
+    ) -> List[SongDomain]:
         pass
