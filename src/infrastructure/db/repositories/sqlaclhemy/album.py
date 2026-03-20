efe4759a22bb678802cedb1dd9964f64e8554e7c
raziel <tvindzon@gmail.com>
2026-03-20 19:44:33 +0700

    feat: src\infrastructure\db\repositories\sqlaclhemy\album.py

--- src/infrastructure/db/repositories/sqlaclhemy/album.py
+++ src/infrastructure/db/repositories/sqlaclhemy/album.py
@@ -14,6 +14,49 @@ class SQLAlchemyAlbumRepository(AlbumRepository):
     def __init__(self, session: AsyncSession):
         self.session: AsyncSession = session
 
+    async def create_album(
+        self,
+        executor_id: int,
+        title: str,
+        year: int,
+        photo_file_id: str,
+        photo_file_unique_id: str,
+    ) -> Album:
+        album = self.model(
+            executor_id=executor_id,
+            title=title,
+            year=year,
+            photo_file_id=photo_file_id,
+            photo_file_unique_id=photo_file_unique_id,
+        )
+        self.session.add(album)
+        await self.session.flush()
+        return album
+
+    async def get_album_by_title(
+        self,
+        executor_id: int,
+        title: str,
+    ) -> Optional[Album]:
+        album: Optional[Album] = await self.session.scalar(
+            select(self.model)
+            .where(
+                self.model.executor_id == executor_id,
+                self.model.title == title,
+            )
+            .options(
+                selectinload(
+                    self.model.songs,
+                )
+            )
+            .options(
+                selectinload(
+                    self.model.executor,
+                )
+            ),
+        )
+        return album
+
     async def get_album(
         self,
         executor_id: int,
