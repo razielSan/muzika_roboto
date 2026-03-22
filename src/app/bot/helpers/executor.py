fbfe9b693a7ccd642b1baa7edacd4ae032746c05
raziel <tvindzon@gmail.com>
2026-03-22 14:38:39 +0700

    feat: src\app\bot\helpers\executor.py

--- src/app/bot/helpers/executor.py
+++ src/app/bot/helpers/executor.py
@@ -1,9 +1,12 @@
 from typing import Callable, Optional
 
 from aiogram import Bot
-from aiogram.types import ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardRemove, CallbackQuery
 
-from app.bot.services.music_library.show_executor_page import ShowExecutorPageService
+from app.bot.services.music_library.show_executor_page import (
+    ShowExecutorPageService,
+    ShowExecutorPageCallbackService,
+)
 from domain.entities.db.uow import AbstractUnitOfWork
 from core.response.response_data import LoggingData
 
@@ -28,7 +31,9 @@ async def return_to_executor_page(
         reply_markup=ReplyKeyboardRemove(),
     )
 
-    await ShowExecutorPageService(uow=uow(), bot=bot, logging_data=logging_data).execute(
+    await ShowExecutorPageService(
+        uow=uow(), bot=bot, logging_data=logging_data
+    ).execute(
         chat_id=chat_id,
         current_page=current_page_executor,
         limit_albums=limit_albums,
@@ -36,4 +41,30 @@ async def return_to_executor_page(
         user_id=user_id,
         album_position=album_position,
         get_information_executor=get_information_executor,
+    )
+
+
+async def return_to_executor_page_callback(
+    call: CallbackQuery,
+    message: str,
+    uow: AbstractUnitOfWork,
+    logging_data: LoggingData,
+    current_page_executor: int,
+    executor_default_photo_file_id: str,
+    limit_albums: int,
+    album_position: int,
+    get_information_executor: Callable,
+    user_id: Optional[int],
+):
+
+    await call.answer(text=message)
+    await ShowExecutorPageCallbackService(
+        uow=uow(), logging_data=logging_data, call=call
+    ).execute(
+        get_information_executor=get_information_executor,
+        executor_default_photo_file_id=executor_default_photo_file_id,
+        user_id=user_id,
+        limit_albums=limit_albums,
+        album_position=album_position,
+        current_page=current_page_executor,
     )
