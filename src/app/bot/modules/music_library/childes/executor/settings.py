5c441a1014fe3a886fa1e8cfe5face33ddb54e85
raziel <tvindzon@gmail.com>
2026-03-27 15:25:16 +0700

    create: module music_library.executor

---
+++ src/app/bot/modules/music_library/childes/executor/settings.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class ModuleSettings(BaseModel):
+    SERVICE_NAME: str = "music_library.childes.executor"
+    MENU_REPLY_TEXT: str = "music_library.childes.executor" 
+    MENU_CALLBACK_TEXT: str = "music_library.childes.executor"
+    MENU_CALLBACK_DATA: str = "music_library.childes.executor"
+    SHOW_IN_MAIN_MENU: bool = True
+    NAME_FOR_LOG_FOLDER: str = "music_library"
+    NAME_FOR_TEMP_FOLDER: str = "music_library/childes/executor"
+    ROOT_PACKAGE: str = "app.bot.modules.music_library.childes.executor"
+    
+settings: ModuleSettings = ModuleSettings()
+    
