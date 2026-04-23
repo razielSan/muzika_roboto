dc2e67714a1b1320f5709bd53ab53ebb138519bb
raziel <tvindzon@gmail.com>
2026-04-24 02:42:42 +0700

    update commit

--- src/app/bot/core/bot.py
+++ src/app/bot/core/bot.py
@@ -8,18 +8,18 @@ def create_bot(
     proxy_settings: ProxySettings,
     logging_data,
 ):
-    proxy = proxy_settings.get_proxy_url()
-    if proxy:
-        proxy_source: str = (
-            "WEBSHARE PROXY"
-            if proxy_settings.USE_WEBSHARE_PROXY
-            else "OTHER PROXY"
-        )
+    # proxy = proxy_settings.get_proxy_url()
+    # if proxy:
+    #     proxy_source: str = (
+    #         "WEBSHARE PROXY"
+    #         if proxy_settings.USE_WEBSHARE_PROXY
+    #         else "OTHER PROXY"
+    #     )
 
-        session: AiohttpSession = AiohttpSession(proxy=proxy)
+    #     session: AiohttpSession = AiohttpSession(proxy=proxy)
 
-        logging_data.info_logger.info(msg=f"[USE {proxy_source}] Прокси подключены")
-        return Bot(token=bot_settings.TOKEN, session=session)
+    #     logging_data.info_logger.info(msg=f"[USE {proxy_source}] Прокси подключены")
+    #     return Bot(token=bot_settings.TOKEN, session=session)
 
     logging_data.info_logger.info("[NOT USED PROXY] Прокси не были использованы")
     return Bot(token=bot_settings.TOKEN)
