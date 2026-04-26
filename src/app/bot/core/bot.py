b859e99d192bf437e8696437801ff5820131a7d9
raziel <tvindzon@gmail.com>
2026-04-26 14:11:54 +0700

    refactor: src\app\bot\core\bot.py

--- src/app/bot/core/bot.py
+++ src/app/bot/core/bot.py
@@ -1,25 +1,25 @@
+from typing import Optional
+
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram import Bot, Dispatcher
-from app.bot.settings import ProxySettings
+
+from app.bot.settings import ProxySettings, proxy_settings
 
 
 def create_bot(
     bot_settings,
-    proxy_settings: ProxySettings,
     logging_data,
+    proxy: Optional[ProxySettings] = None,
 ):
-    # proxy = proxy_settings.get_proxy_url()
-    # if proxy:
-    #     proxy_source: str = (
-    #         "WEBSHARE PROXY"
-    #         if proxy_settings.USE_WEBSHARE_PROXY
-    #         else "OTHER PROXY"
-    #     )
+    if proxy:
+        proxy_source: str = (
+            "WEBSHARE PROXY" if proxy_settings.USE_WEBSHARE_PROXY else "OTHER PROXY"
+        )
 
-    #     session: AiohttpSession = AiohttpSession(proxy=proxy)
+        session: AiohttpSession = AiohttpSession(proxy=proxy)
 
-    #     logging_data.info_logger.info(msg=f"[USE {proxy_source}] Прокси подключены")
-    #     return Bot(token=bot_settings.TOKEN, session=session)
+        logging_data.info_logger.info(msg=f"[USE {proxy_source}] Прокси подключены")
+        return Bot(token=bot_settings.TOKEN, session=session)
 
     logging_data.info_logger.info("[NOT USED PROXY] Прокси не были использованы")
     return Bot(token=bot_settings.TOKEN)
